ce6cf94aff8ba49e7bdc4759d83973f1206ce23b
Cniu <cniu@bu.edu>
2020-04-17 18:53:26 -0400

    update

--- Boruta_Feature_Selection.R
+++ Boruta_Feature_Selection.R
@@ -111,12 +111,10 @@ stock_df <- attStats(boruta_stock)
 stock_df_extra <- attStats(boruta_stock_extra)
 
 ## save results in the environment
-#save.image(file='Boruta_feature_selection_results.RData')
 save.image(file='Boruta_feature_selection_results_version1.RData')
 ## check if its in the current dictory
 dir()
 ## load pervious saved results
-#load('Boruta_feature_selection_results.RData')
 load('Boruta_feature_selection_results_version1.RData')
 
 
