90ad4881703530c12c8d7ea00443bc3bbb0afbd6
Cniu <cniu@bu.edu>
2020-04-16 21:13:41 -0400

    Boruta

--- Boruta_Feature_Selection.R
+++ Boruta_Feature_Selection.R
@@ -39,11 +39,49 @@ ggplot_missing<- function(x){
 ggplot_missing(read_file) 
 ## no white lines shows in the graph which means there is no missing values
 
-##
+## set seeds for reproduction
 set.seed(888)
-##
-boruta_stock_train <- Boruta(Class~., data =read_file[-c(1,2,227)], doTrace = 3, maxRuns=200)# don't even try to run it, take forever
+## feature selections 
+boruta_stock_train <- Boruta(Class~., data =read_file[-c(1,2,227)], doTrace = 3)# don't even try to run it, take forever
 
-boruta_stock_train_extra <- Boruta(Class~., data =read_file[-c(1,2,227)], doTrace = 3, maxRuns=200)
+## boruta_stock_train_extra <- Boruta(Class~., data =read_file[-c(1,2,227)], doTrace = 3, maxRuns=200)## can do more Runs with maxRuns specified 
 
 print(boruta_stock_train)
+boruta_stock_train$finalDecision[boruta_stock_train$finalDecision[]=="Confirmed"]
+length(boruta_stock_train$finalDecision[boruta_stock_train$finalDecision[]=="Confirmed"])
+
+## since we still have 64 tentative attributes left, let check these variables
+## here Tentative features have an importance that is so close to their best shadow features that Boruta is not able to 
+## make a decision with the desired confidence in the default number of Random Forest runs.
+#take a call on tentative features
+boruta_stock <- TentativeRoughFix(boruta_stock_train)
+print(boruta_stock)
+## now boruta down it's work
+
+##Plot out results
+## The y axis label Importance represents the Z score of every feature in the shuffled dataset.
+## The blue boxplots correspond to minimal, average and maximum Z score of a shadow feature, 
+## while the red and green boxplots represent Z scores of rejected and confirmed features, respectively. 
+## As you can see the red boxplots have lower Z score than that of maximum Z score of shadow feature which is precisely 
+## the reason they were put in unimportant category.
+plot(boruta_stock, xlab = "", xaxt = "n")
+lz<-lapply(1:ncol(boruta_stock$ImpHistory),function(i)
+  boruta_stock$ImpHistory[is.finite(boruta_stock$ImpHistory[,i]),i])
+names(lz) <- colnames(boruta_stock$ImpHistory)
+Labels <- sort(sapply(lz,median))
+axis(side = 1,las=2,labels = names(Labels),
+     at = 1:ncol(boruta_stock$ImpHistory), cex.axis = 0.7)
+
+##confirm the importance of the features
+getSelectedAttributes(boruta_stock, withTentative = F)
+##store results into a dataframe
+stock_df <- attStats(boruta_stock)
+
+## save results in the environment
+save.image(file='Boruta_feature_selection_results.RData')
+## check if its in the current dictory
+dir()
+## load pervious saved results
+load('Boruta_feature_selection_results.RData')
+
+
