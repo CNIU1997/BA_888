5d0234225cb974354b36f557952cf6cc6f41489a
Cniu <cniu@bu.edu>
2020-04-24 09:25:52 -0400

    update

--- Boruta_Feature_Selection.R
+++ Boruta_Feature_Selection.R
@@ -6,6 +6,12 @@ library(Boruta)
 library(Amelia)
 library(RColorBrewer)
 library(aplot)
+library(tidyverse)
+library(olsrr)
+library(party)
+install.packages("relaimpo")
+library(relaimpo)
+
 ##
 read_file<- read.csv("train_data.csv")
 
@@ -19,6 +25,21 @@ classVariables = sapply(read_file, function(x) class(x))
 length(names(which(sapply(read_file, class) == "factor"))) 
 length(names(which(sapply(read_file, class) == "numeric"))) 
 length(names(which(sapply(read_file, class) == "character"))) 
+
+
+## Random Forest Method,find a set of predictors that best explains the variance in the response variable.
+df_rf= read_file[-c(1,2,225,227)]
+cf1 <- cforest(Class ~ . , data= df_rf, control=cforest_unbiased(mtry=2,ntree=50))
+RF_variable_importance<- varimp(cf1) # get variable importance, based on mean decrease in accuracy
+varimp(cf1, conditional=TRUE)  # conditional=True, adjusts for correlations between predictors
+varimpAUC(cf1)  # more robust towards class imbalance.
+
+## Relative Importance, the relative importance of variables fed into a lm model can be determined as a relative percentage.
+lmMod <- lm(Class ~ . , data = df_rf)  # fit lm() model
+relImportance <- calc.relimp(lmMod, type = "lmg", rela = TRUE)  # calculate relative importance scaled to 100
+sort(relImportance$lmg, decreasing=TRUE)  # relative importance
+
+
 
 ##
 read_file[-226][is.na(read_file[-226])] =0
@@ -44,6 +65,7 @@ ggplot_missing(read_file)
 
 ## set seeds for reproduction
 set.seed(888)
+## It is a binary classification problem with multiple features.
 ## feature selections 
 boruta_stock_train <- Boruta(Class~., data =read_file[-c(1,2,225,227)], doTrace = 3)# don't even try to run it, take forever
 #
@@ -102,12 +124,12 @@ axis(side = 1,las=2,labels = names(Labels),
      at = 1:ncol(boruta_stock_extra$ImpHistory), cex.axis = 0.8,hadj =0.25)
 
 ##confirm the importance of the features
-getSelectedAttributes(boruta_stock, withTentative = F)
+getSelectedAttributes(boruta_stock, withTentative = T)
 #
-getSelectedAttributes(boruta_stock_extra, withTentative = F)
+getSelectedAttributes(boruta_stock_extra, withTentative = T)
 ##store results into a dataframe
 stock_df <- attStats(boruta_stock)
-#
+# 
 stock_df_extra <- attStats(boruta_stock_extra)
 
 ## save results in the environment
@@ -117,4 +139,49 @@ dir()
 ## load pervious saved results
 load('Boruta_feature_selection_results_version1.RData')
 
+
+##
+feature_selected <- cbind(variable = rownames(stock_df_extra), stock_df_extra)
+rownames(feature_selected) <- 1:nrow(feature_selected)
+feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>7) %>% select(variable)# top27_features
+feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>6) %>% select(variable)# top57_features
+
+df_test$niperEBT==df_test$nIperEBT # same columns 
+df_test$eBTperEBIT== df_test$ebtperEBIT #Same columns
+df_test$Net_Income_Com== df_test$Net_Income ## Not the same columns 
+df_test$ priceToBookRatio== df_test$PB_ratio ## Not the same columns
+df_test$Net_Profit_Margin== df_test$netProfitMargin ## Not the same columns
+##
+df_test<- read.csv("test_data.csv")
+glimpse(df_test)
+summary(df_test)
+#top27 accuarcy : 0.6833 
+classifier <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+
+                    SG.A_to_Revenue+ priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+
+                    Sector+ Gross_Profit_Growth+ EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+
+                    eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ Net_Income+
+                    Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
+                    Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio, 
+                  family='binomial', data =read_file[-c(1,2,225,227)])
+summary(classifier)
+N_test= nrow(df_test)
+predictions <- predict(classifier,newdata=df_test[-c(1,2,225,227)],type="response")[1:N_test]
+confusionMatrix(factor(round(predictions)),factor(df_test['Class'][1:N_test,]))
+
+#top 57 accuarcy: : 0.5162  
+classifier_1 <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+ SG.A_to_Revenue+ 
+                     priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+ Sector+ Gross_Profit_Growth+ 
+                     EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+ eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ 
+                     Net_Income+ Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+ 
+                     Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio+ 
+                     Free_Cash_Flow_Yield+ Consolidated_Income+ Profit_Margin+ priceToBookRatio+ PB_ratio+ priceBookValueRatio+ 
+                     PTB_ratio+ Earnings_before_Tax+ operatingCashFlowSalesRatio+ enterpriseValueMultiple+ grossProfitMargin+ 
+                     Net_Profit_Margin+ netProfitMargin+ Gross_Margin+ Net_Income_per_Share+ EBIT_Margin+ eBITperRevenue+ 
+                     Graham_Number+ ebitperRevenue+ priceSalesRatio+ EBIT+ pretaxProfitMargin+ Operating_Cash_Flow+ 
+                     Total_non.current_assets+ Free_Cash_Flow_margin+ EBITDA+ Income_Quality+EBITDA_Margin+ 
+                     Book_Value_per_Share_Growth+ dividendpaidAndCapexCoverageRatios,
+                   family='binomial', data =read_file[-c(1,2,225,227)])
+summary(classifier_1)
+predictions_1 <- predict(classifier_1,newdata=df_test[-c(1,2,225,227)],type="response")[1:N_test]
+confusionMatrix(factor(round(predictions_1)),factor(df_test['Class'][1:N_test,]))
 
