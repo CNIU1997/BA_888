8e5ef0e5b0be957d454ab0d8b46d9c0062115bea
Cniu <cniu@bu.edu>
2020-04-26 12:08:38 -0400

    changes

--- Boruta_Feature_Selection.R
+++ Boruta_Feature_Selection.R
@@ -9,8 +9,11 @@ library(aplot)
 library(tidyverse)
 library(olsrr)
 library(party)
-install.packages("relaimpo")
 library(relaimpo)
+library(pROC)
+library(plyr)
+library(caret)
+library(VGAM)
 
 ##
 read_file<- read.csv("train_data.csv")
@@ -135,8 +138,8 @@ stock_df_extra <- attStats(boruta_stock_extra)
 ## test the selected variables performance 
 feature_selected <- cbind(variable = rownames(stock_df_extra), stock_df_extra)
 rownames(feature_selected) <- 1:nrow(feature_selected)
-feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>7) %>% select(variable)# top27_features
-feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>6) %>% select(variable)# top57_features
+feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>7)# top27_features
+feature_selected %>% filter(decision=="Confirmed") %>% arrange(desc(meanImp))%>% filter(meanImp>6)# top57_features
 
 ##load test dataset
 df_test<- read.csv("test_data.csv")
@@ -150,35 +153,88 @@ df_test$Net_Income_Com== df_test$Net_Income ## Not the same columns
 df_test$ priceToBookRatio== df_test$PB_ratio ## Not the same columns
 df_test$Net_Profit_Margin== df_test$netProfitMargin ## Not the same columns
 
+
+##Convert Class to a factor (and don't use "0" and "1" as levels) 
+class(read_file$Class)
+## drop any value in class not equal to 1 or o
+read_file<-read_file[(read_file$Class=="1" | read_file$Class=="0"),]
+## 
+read_file$Class <- read_file$Class[read_file$Class != c("True","X1")]
+summary(read_file$Class)
+read_file$Class <- droplevels(read_file$Class)
+## reorder the factor level
+read_file$Class<- factor(read_file$Class, levels=rev(levels(read_file$Class)))
+levels(read_file$Class)
+##Convert Class to a factor (and don't use "0" and "1" as levels) 
+class(df_test$Class)
+df_test$Class<- as.factor(df_test$Class)
+## drop any value in class not equal to 1 or o
+df_test<-df_test[(df_test$Class=="1" | df_test$Class=="0"),]
+## 
+df_test$Class <- df_test$Class[df_test$Class != c("True","X1")]
+summary(df_test$Class)
+df_test$Class <- droplevels(df_test$Class)
+## reorder the factor level
+df_test$Class<- factor(df_test$Class, levels=rev(levels(df_test$Class)))
+levels(df_test$Class)
+
+
 #top27 features  accuarcy : 0.6833 
-classifier <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+
+classifier <- vglm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+
                     SG.A_to_Revenue+ priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+
                     Sector+ Gross_Profit_Growth+ EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+
                     eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ Net_Income+
                     Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
                     Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio, 
-                  family='binomial', data =read_file[-c(1,2,225,227)])
+                  family='multinomial', data =read_file[-c(1,2,225,227)])
+
 summary(classifier)
 N_test= nrow(df_test)
 predictions <- predict(classifier,newdata=df_test[-c(1,2,225,227)],type="response")[1:N_test]
-confusionMatrix(factor(round(predictions)),factor(df_test['Class'][1:N_test,]))
+predictions<- round(predictions) %>% as.factor()
+## reorder the factor level
+predictions<- factor(predictions, levels=rev(levels(predictions)))
+levels(predictions)
+confusionMatrix(factor(predictions),factor(df_test['Class'][1:N_test,]))
 
-#top57 features accuarcy: : 0.5162  
-classifier_1 <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+ SG.A_to_Revenue+ 
-                     priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+ Sector+ Gross_Profit_Growth+ 
-                     EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+ eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ 
-                     Net_Income+ Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+ 
-                     Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio+ 
-                     Free_Cash_Flow_Yield+ Consolidated_Income+ Profit_Margin+ priceToBookRatio+ PB_ratio+ priceBookValueRatio+ 
-                     PTB_ratio+ Earnings_before_Tax+ operatingCashFlowSalesRatio+ enterpriseValueMultiple+ grossProfitMargin+ 
-                     Net_Profit_Margin+ netProfitMargin+ Gross_Margin+ Net_Income_per_Share+ EBIT_Margin+ eBITperRevenue+ 
-                     Graham_Number+ ebitperRevenue+ priceSalesRatio+ EBIT+ pretaxProfitMargin+ Operating_Cash_Flow+ 
-                     Total_non.current_assets+ Free_Cash_Flow_margin+ EBITDA+ Income_Quality+EBITDA_Margin+ 
-                     Book_Value_per_Share_Growth+ dividendpaidAndCapexCoverageRatios,
-                   family='binomial', data =read_file[-c(1,2,225,227)])
+
+#top42 features accuarcy : 0.679 (the largetest model vglm with multinomial can handel)
+classifier_1 <- vglm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+ SG.A_to_Revenue+
+                      priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+ Sector+ Gross_Profit_Growth+
+                      EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+ eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+
+                      Net_Income+ Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
+                      Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio+
+                      Free_Cash_Flow_Yield+ Consolidated_Income+ Profit_Margin+ priceToBookRatio+ PB_ratio+ priceBookValueRatio+
+                      PTB_ratio+ Earnings_before_Tax+ operatingCashFlowSalesRatio+ enterpriseValueMultiple+ grossProfitMargin+
+                      Net_Profit_Margin+ netProfitMargin+ Gross_Margin+ Net_Income_per_Share+ EBIT_Margin+ eBITperRevenue+
+                      Graham_Number,
+                    family='multinomial', data =read_file[-c(1,2,225,227)])
 summary(classifier_1)
 predictions_1 <- predict(classifier_1,newdata=df_test[-c(1,2,225,227)],type="response")[1:N_test]
-confusionMatrix(factor(round(predictions_1)),factor(df_test['Class'][1:N_test,]))
+predictions_1<- round(predictions_1) %>% as.factor()
+## reorder the factor level
+predictions_1<- factor(predictions_1, levels=rev(levels(predictions_1)))
+levels(predictions_1)
+confusionMatrix(predictions_1,df_test['Class'][1:N_test,])
+
+## The most accuarte model so far
+## Accuracy : 0.6849 
+## from PTB_ratio to Profit_Margin accuracy stays the same
+classifier_2 <- vglm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+
+                       SG.A_to_Revenue+ priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+
+                       Sector+ Gross_Profit_Growth+ EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+
+                       eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ Net_Income+
+                       Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
+                       Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio+
+                       Free_Cash_Flow_Yield+ Consolidated_Income+ Profit_Margin+ priceToBookRatio+ PB_ratio+ priceBookValueRatio+ PTB_ratio,
+                     family='multinomial', data =read_file[-c(1,2,225,227)])
+summary(classifier_2)
+predictions_2 <- predict(classifier_2,newdata=df_test[-c(1,2,225,227)],type="response")[1:N_test]
+predictions_2<- round(predictions_2) %>% as.factor()
+## reorder the factor level
+predictions_2<- factor(predictions_2, levels=rev(levels(predictions_2)))
+levels(predictions_2)
+confusionMatrix(predictions_2,df_test['Class'][1:N_test,])
 
 ## save results in the environment
 save.image(file='Boruta_feature_selection_results_version1.RData')
@@ -186,4 +242,45 @@ save.image(file='Boruta_feature_selection_results_version1.RData')
 dir()
 ## load pervious saved results
 load('Boruta_feature_selection_results_version1.RData')
+
+
+##
+pROC_obj <- roc(df$labels,df$predictions,
+                smoothed = TRUE,
+                # arguments for ci
+                ci=TRUE, ci.alpha=0.9, stratified=FALSE,
+                # arguments for plot
+                plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
+                print.auc=TRUE, show.thres=TRUE)
+
+
+# top 27 features
+# classifier <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+
+#                     SG.A_to_Revenue+ priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+
+#                     Sector+ Gross_Profit_Growth+ EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+
+#                     eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+ Net_Income+
+#                     Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
+#                     Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio, 
+#                   family='binomial', data =read_file[-c(1,2,225,227)])
+
+#top57 features accuarcy: : 0.5162  
+# classifier_1 <- glm(Class~niperEBT+ Effect_of_forex_changes_on_cash+ Earnings_Yield+ effectiveTaxRate+ SG.A_to_Revenue+
+#                      priceFairValue+ Weighted_Average_Shares_Diluted_Growth+ EV_to_Free_cash_flow+ Sector+ Gross_Profit_Growth+
+#                      EV_to_Operating_cash_flow+ Weighted_Average_Shares_Growth+ eBTperEBIT+ assetTurnover+ EV_to_Sales+ Net_Income_Com+
+#                      Net_Income+ Enterprise_Value_over_EBITDA+ Revenue_Growth+ Operating_Cash_Flow_per_Share+ Inventory_Growth+
+#                      Earnings_Before_Tax_Margin+ operatingCashFlowPerShare+ priceToOperatingCashFlowsRatio+ POCF_ratio+
+#                      Free_Cash_Flow_Yield+ Consolidated_Income+ Profit_Margin+ priceToBookRatio+ PB_ratio+ priceBookValueRatio+
+#                      PTB_ratio+ Earnings_before_Tax+ operatingCashFlowSalesRatio+ enterpriseValueMultiple+ grossProfitMargin+
+#                      Net_Profit_Margin+ netProfitMargin+ Gross_Margin+ Net_Income_per_Share+ EBIT_Margin+ eBITperRevenue+
+#                      Graham_Number+ ebitperRevenue+ priceSalesRatio+ EBIT+ pretaxProfitMargin+ Operating_Cash_Flow+
+#                      Total_non.current_assets+ Free_Cash_Flow_margin+ EBITDA+ Income_Quality+EBITDA_Margin+
+#                      Book_Value_per_Share_Growth+ dividendpaidAndCapexCoverageRatios,
+#                    family='binomial', data =read_file[-c(1,2,225,227)])
+
+
+#df_test$Class<- revalue(df_test$Class, c("1"="Buy", "0"="Not Buy"))
+#predictions<- revalue(predictions, c("1"="Buy", "0"="Not Buy"))
+
+
+
 
