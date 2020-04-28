17cc328b460e94c7b16e184e698efee0937fe7ae
Cniu <cniu@bu.edu>
2020-04-27 23:45:23 -0400

    changes

--- PCA_with Boruta Results.R
+++ PCA_with Boruta Results.R
@@ -9,7 +9,7 @@ library(factoextra)
 library(cluster)
 library(radarchart)
 library(fmsb)
-
+library(grDevices)
 # 'niperEBT',
 # 'Effect_of_forex_changes_on_cash',
 # 'Earnings_Yield',
@@ -18,7 +18,6 @@ library(fmsb)
 # 'priceFairValue',
 # 'Weighted_Average_Shares_Diluted_Growth',
 # 'EV_to_Free_cash_flow',
-# 'Sector',
 # 'Gross_Profit_Growth',
 # 'EV_to_Operating_cash_flow',
 # 'Weighted_Average_Shares_Growth',
@@ -48,7 +47,6 @@ library(fmsb)
 train= read.csv('train_data.csv')
 dim(train)
 train<- na.omit(train)
-colnames(train)
 
 #Remove Redundant Features
 select_names <- c('niperEBT',
@@ -113,7 +111,7 @@ c_pca6 <- c_pcs[,1:6]
 c_scale <- scale(c_pca6)
 
 #eval numer of k using silhouette and wss
-fviz_nbclust(c_scale, kmeans, method = 'wss', k.max = 30)
+fviz_nbclust(c_scale, kmeans, method = 'wss', k.max = 10)
 
 k_wss = function(k) {
   km = kmeans(c_scale, k, nstart=25, iter=25)
@@ -125,32 +123,32 @@ x <- 1:30
 wass <- map_dbl(x,k_wss)
 plot(x,wass, type ="b") # 4
 
-fviz_nbclust(c_scale, kmeans, method = 'silhouette', k.max = 20)#silhouette suggests 2 clusters
+fviz_nbclust(c_scale, kmeans, method = 'silhouette', k.max = 10)#silhouette suggests 2 clusters
 
 #Kmeans k of 2/3/4/5/6
 pca_k2 <- kmeans(c_scale,2,25,30)
 pca_k3 <- kmeans(c_scale,3,25,30)
 pca_k4 <- kmeans(c_scale,4,25,30)
 pca_k5 <- kmeans(c_scale,5,25,30)
-# pca_k6 <- kmeans(c_scale,6,25,30)
+pca_k6 <- kmeans(c_scale,6,25,30)
 # 
 table(pca_k2$cluster)
 table(pca_k3$cluster)
 table(pca_k4$cluster)
 table(pca_k5$cluster)
-# table(pca_k6$cluster)
+table(pca_k6$cluster)
 # 
 pca_k2$tot.withinss
 pca_k3$tot.withinss
 pca_k4$tot.withinss
 pca_k5$tot.withinss
-# pca_k6$tot.withinss
+pca_k6$tot.withinss
 
 fviz_cluster(pca_k2,select_data)
 fviz_cluster(pca_k3,select_data)
 fviz_cluster(pca_k4,select_data)
 fviz_cluster(pca_k5,select_data)
-# fviz_cluster(pca_k6,select_data))
+fviz_cluster(pca_k6,select_data)
 
 # final decision: 2 clusters
 ##adding back clusters and 2 pca variables to original dataset
@@ -173,27 +171,30 @@ data_summary<- final_data %>%
   dplyr::select(-starts_with("PC"))%>% as.data.frame() 
 summary<-summary(data_summary)
 dim(summary)
-max<- summary[6,1:16]
+max<- summary[6,1:32]
 max<-as.numeric(gsub("Max.   :", "", max))
 max
-data_summary <- rbind(max, rep(0,223), data_summary)
+data_summary <- rbind(max, rep(-25,32), data_summary)
 data_summary$cluster<-NULL
 ###############
 
 #cluster_1
 cluster_1<-data_summary[c(1:2,3),]
 radarchart(cluster_1, 
-           
+           pcol=rgb(0.1,0.4,0.7,0.9),
+           pfcol=rgb(0.1,0.8,1,0.5),
            #custom the grid
            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
            
            #custom labels
-           vlcex=1
+           vlcex=0.8
 )
+
 #cluster_2
 cluster_2<-data_summary[c(1:2,4),]
 radarchart(cluster_2, 
-           
+           pcol=rgb(0.1,0.4,0.7,0.9),
+           pfcol=rgb(0.1,0.8,1,0.5),
            #custom the grid
            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
            
